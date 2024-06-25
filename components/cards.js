b047fccecdb0b2ba8c14f30bf76c9153b71a2527
BabaikaSiberienne <awesome.flure@yandex.ru>
2024-06-25 23:34:54 +0300

    cards added

---
+++ components/cards.js
@@ -0,0 +1,64 @@
+cards = [
+    {
+        title: "Приключение на 20 минут",
+        alt: "горы",
+        link: "./images/priklucheniye_na_20_minut_1.jpg"
+    },
+    {
+        title: "Приключение на 20 минут",
+        alt: "равнина в горах",
+        link: "./images/priklucheniye_na_20_minut_2.jpg"
+    },
+    {
+        title: "Приключение на 20 минут",
+        alt: "люди идут в гору",
+        link: "./images/priklucheniye_na_20_minut_3.jpg"
+    },
+    {
+        title: "Ознакомительный",
+        alt: "горы",
+        link: "./images/oznakomitelniy_1.jpg"
+    },
+    {
+        title: "Ознакомительный",
+        alt: "палатка на равнине в горах",
+        link: "./images/oznakomitelniy_2.jpg"
+    },
+    {
+        title: "Ознакомительный",
+        alt: "горная река",
+        link: "./images/oznakomitelniy_3.jpg"
+    },
+    {
+        title: "Галопом по хибинам",
+        alt: "горная река, закат",
+        link: "./images/galopom_po_hibinam_1.jpg"
+    },
+    {
+        title: "Галопом по хибинам",
+        alt: "горы",
+        link: "./images/galopom_po_hibinam_2.jpg"
+    },
+    {
+        title: "Галопом по хибинам",
+        alt: "горная река в долине",
+        link: "./images/galopom_po_hibinam_3.jpg"
+    },
+    {
+        title: "Лайт",
+        alt: "деревня, закат",
+        link: "./images/lite_1.jpg"
+    },
+    {
+        title: "Лайт",
+        alt: "горная река днём",
+        link: "./images/lite_2.jpg"
+    },
+    {
+        title: "Лайт",
+        alt: "равнина в горах",
+        link: "./images/lite_3.jpg"
+    }
+
+]
+
