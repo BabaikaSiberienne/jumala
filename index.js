3e068eb748fd3e4aa91b5adf29ba5d468b11e365
Jenia Shalomeenko <evgeniyashalomeenko@yandex.ru>
2024-06-26 00:00:35 +0300

    changed html - added template and script for js, styled list of cards, added initial js

---
+++ index.js
@@ -0,0 +1,39 @@
+const programCards = [{
+  title: "Приключение на 20 минут",
+  alt: "иллюстрация предлагаемого тура",
+  link: "./images/Gory.jpg"
+},
+{
+  title: "Приключение на 20 минут",
+  alt: "иллюстрация предлагаемого тура",
+  link: "./images/Gory.jpg"},
+{
+    title: "Приключение на 20 минут",
+    alt: "иллюстрация предлагаемого тура",
+    link: "./images/Gory.jpg"}
+  
+]
+
+
+console.log(programCards)
+
+const cardTemplate = document.querySelector('#main__tour-template');              //шаблон картинки программы
+console.log(cardTemplate);
+const cardContainer = document.querySelector('.main__tours-list');                //место для включения картинок
+let tourCard = cardTemplate.content.querySelector('.main__one-tour');             //карточка тура в разметке
+
+function createCard(data) {
+  const tourCardItem = tourCard.cloneNode(true);                                  //клонировать шаблон 
+  const tourImage = tourCardItem.querySelector('.main__tour-image');              //картинка карточки тура
+  const tourName = tourCardItem.querySelector('.main__tour-name');                //название тура
+  tourImage.src = data.link;
+  tourImage.alt = data.alt;
+  tourName.textContent = data.title;
+  return tourCardItem
+}
+
+programCards.forEach(item => {
+  const tourCard = createCard(item);
+  cardContainer.prepend(tourCard)
+});
+
