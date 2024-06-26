2e18c6e2b26cad94ecc5b6358599a4477c49b7b0
BabaikaSiberienne <awesome.flure@yandex.ru>
2024-06-26 22:54:05 +0300

    file deleted

--- components/cards_programs.js
+++
@@ -1,9 +0,0 @@
-export const programCards = [{
-  title: "Приключение на 20 минут",
-  alt: "иллюстрация предлагаемого тура",
-  link: "../images/Gory.jpg"
-},
-{
-  title: "Приключение на 20 минут",
-  alt: "иллюстрация предлагаемого тура",
-  link: "../images/Gory.jpg"}]
