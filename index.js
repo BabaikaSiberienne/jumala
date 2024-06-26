381e68598298e24dc75edb7b1b5c334ca58b192a
BabaikaSiberienne <awesome.flure@yandex.ru>
2024-06-26 22:56:04 +0300

    import added

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-const programCards = [{
-  title: "Приключение на 20 минут",
-  alt: "иллюстрация предлагаемого тура",
-  link: "./images/Gory.jpg"
-},
-{
-  title: "Приключение на 20 минут",
-  alt: "иллюстрация предлагаемого тура",
-  link: "./images/Gory.jpg"},
-{
-    title: "Приключение на 20 минут",
-    alt: "иллюстрация предлагаемого тура",
-    link: "./images/Gory.jpg"}
-  
-]
+import {cards} from "./components/cards.js"
 
 
 console.log(programCards)
