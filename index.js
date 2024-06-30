ef9ad1bdd836c8168164143385b379c95b1e5dc9
Jenia Shalomeenko <evgeniyashalomeenko@yandex.ru>
2024-06-30 11:16:27 +0300

    added description

--- index.js
+++ index.js
@@ -1,39 +1,53 @@
 const programCards = [{
   title: "Приключение на 20 минут",
   alt: "иллюстрация предлагаемого тура",
+  text: "описание тура",
   link: "./images/Gory.jpg"
 },
 {
   title: "Приключение на 20 минут",
   alt: "иллюстрация предлагаемого тура",
+  text: "описание тура",
   link: "./images/Gory.jpg"},
 {
     title: "Приключение на 20 минут",
     alt: "иллюстрация предлагаемого тура",
+    text: "описание тура",
     link: "./images/Gory.jpg"}
   
 ]
 
-
-console.log(programCards)
-
 const cardTemplate = document.querySelector('#main__tour-template');              //шаблон картинки программы
-console.log(cardTemplate);
 const cardContainer = document.querySelector('.main__tours-list');                //место для включения картинок
 let tourCard = cardTemplate.content.querySelector('.main__one-tour');             //карточка тура в разметке
 
-function createCard(data) {
+
+const tourDescription = document.querySelector('.main__tour-description');
+
+function createCard(data, onTurnOn) {
   const tourCardItem = tourCard.cloneNode(true);                                  //клонировать шаблон 
   const tourImage = tourCardItem.querySelector('.main__tour-image');              //картинка карточки тура
   const tourName = tourCardItem.querySelector('.main__tour-name');                //название тура
+  const tourDescription = tourCardItem.querySelector('.main__tour-description');
+  const descriptionText = tourCardItem.querySelector('.main__tour-text');
+
   tourImage.src = data.link;
   tourImage.alt = data.alt;
   tourName.textContent = data.title;
+
+  tourCardItem.addEventListener('click', onTurnOn);
+  // descriptionText.addEventListener('click', onTurnOff);
+
   return tourCardItem
 }
 
-programCards.forEach(item => {
-  const tourCard = createCard(item);
-  cardContainer.prepend(tourCard)
-});
+function onTurnOn(event){
+  const tourCard = event.target.closest('.main__one-tour');
+  const tourItem = tourCard.querySelector('.tour-box')
+  tourItem.classList.toggle('description-disabled');
+}
 
+programCards.forEach(function (item) {
+  const tourCard = createCard(item, onTurnOn);
+  cardContainer.prepend(tourCard);
+});
